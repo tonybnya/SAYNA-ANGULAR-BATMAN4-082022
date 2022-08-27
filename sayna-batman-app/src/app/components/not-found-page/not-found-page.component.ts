daff2283e8cd36498ae1e9bc5b2f27db50f0e13f
Tony <nya.tony2010@gmail.com>
2022-08-27 17:04:37 +0100

    review & 404 page component integration, styling & routing of the buttons & design of the toggle button on the navbar

--- sayna-batman-app/src/app/components/not-found-page/not-found-page.component.ts
+++ sayna-batman-app/src/app/components/not-found-page/not-found-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onHome(): void {
+    this.router.navigateByUrl('home');
+  }
+
+  onEshop(): void {
+    this.router.navigateByUrl('eshop');
   }
 
 }
