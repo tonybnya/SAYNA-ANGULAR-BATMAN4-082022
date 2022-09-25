5f71ff1ccd4777d7c8d265236832990e2e103297
Tony <nya.tony2010@gmail.com>
2022-09-25 11:47:47 +0100

    add items to the cart

--- sayna-batman-app/src/app/components/products/products.component.ts
+++ sayna-batman-app/src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 import { ApiArticlesService } from 'src/app/services/api-articles.service';
 
 @Component({
@@ -9,13 +11,22 @@ import { ApiArticlesService } from 'src/app/services/api-articles.service';
 export class ProductsComponent implements OnInit {
   page: number = 1;
   public products: any;
+  isAdded: boolean = false;
 
-  constructor(private api: ApiArticlesService) { }
+  constructor(private api: ApiArticlesService, private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
     this.api.getArticles().subscribe((res) => { 
       this.products = res;
+      this.products.forEach((a: any) => {
+        Object.assign(a, { quantity: 1, total: a.prix });
+      });
     });
+  }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+    this.router.navigateByUrl('cart');
   }
 
 }
