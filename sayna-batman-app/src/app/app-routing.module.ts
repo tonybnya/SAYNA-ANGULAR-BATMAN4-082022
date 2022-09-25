dc3c800395e52f3ae674aee6b775716592047ad6
Tony <nya.tony2010@gmail.com>
2022-09-25 17:13:01 +0100

    design mon-compte template

--- sayna-batman-app/src/app/app-routing.module.ts
+++ sayna-batman-app/src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { CommandComponent } from './components/command/command.component';
 import { EshopComponent } from './components/eshop/eshop.component';
 import { GameComponent } from './components/game/game.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { SignupComponent } from './components/signup/signup.component';
 import { SingleProductComponent } from './components/single-product/single-product.component';
 
 const routes: Routes = [
@@ -18,6 +20,8 @@ const routes: Routes = [
   {path: 'cart', component: CartComponent },
   {path: 'eshop/product/:id', component: SingleProductComponent },
   { path:'cart/command', component:CommandComponent },
+  { path:'login', component:LoginComponent },
+  { path:'signup', component:SignupComponent },
   { path: '**', component: NotFoundPageComponent }
 ];
 
--- sayna-batman-app/src/app/components/login/login.component.ts
+++ sayna-batman-app/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder, private router:Router, private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password:['']
+    });
+  }
+
+  login(){
+
+    // this.authService.login().subscribe((res)=>{
+    //   const utilisateur = res.find((ans:any)=>{
+    //     return ans.email === this.loginForm.value.email && ans.password === this.loginForm.value.password;
+    //   });
+
+    //   if(utilisateur){
+    //     alert('Vous etes connecte');
+    //     this.loginForm.reset();
+    //     //suisconnecte = true;
+    //     this.authService.suisConnecte = true;
+    //     this.router.navigateByUrl('/cart/commande');
+    //   }
+    //   else{
+    //     alert('Veuillez vous enregistrer');
+    //   }
+    // })
   }
 
 }
